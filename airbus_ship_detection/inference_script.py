5e1932fad16cc1a3b96a9a5ca20f44be31bdeb68
KharchenkoAnastasia <kharchenko.13.08@gmail.com>
2024-08-20 02:08:38 +0300

    Update inference_script.py

--- airbus_ship_detection/inference_script.py
+++ airbus_ship_detection/inference_script.py
@@ -1,4 +1,5 @@
-import os
+from pathlib import Path
+from typing import Union
 import sys
 
 import numpy as np
@@ -8,38 +9,42 @@ from PIL import Image
 from airbus_ship_detection.utils.loss import loss
 
 # Load the saved model
-model_path = os.path.join('../model', 'unet_model.h5')
-unet_model = tf.keras.models.load_model(model_path,custom_objects={'loss': loss})
-
-def main(directory: str) -> None:
-        # Create the "image_mask" folder if it doesn't exist
-        mask_folder = os.path.join(directory, "image_mask")
-        os.makedirs(mask_folder, exist_ok=True)
-        
-        # Find all image files in the directory
-        image_files = [f for f in os.listdir(directory) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
-        
-        
-        # Perform segmentation and save the results      
-        for image_file in image_files:
-            image_path  = os.path.join(directory, image_file)
-            image = Image.open(image_path).resize((256, 256))
-            image_array = np.expand_dims(image, 0) / 255.0
-            segmentation = unet_model.predict(image_array)
-                
-            # Save the mask segmentation image
-            mask_image = (segmentation[0, :, :, 0] * 255).astype(np.uint8)
-            save_path = os.path.join(mask_folder, f'{os.path.splitext(image_file)[0]}_mask.png')
-            pil_image = Image.fromarray(np.uint8(mask_image))
-            pil_image.save(save_path)
-            print(f'Saved mask segmentation for {image_file}')
+model_path = Path("../model") / "unet_model.h5"
+unet_model = tf.keras.models.load_model(model_path, custom_objects={"loss": loss})
 
 
-if __name__ == '__main__':
+def main(directory: Union[str, Path]) -> None:
+    print(f"Processing directory: {directory}")
+    directory = Path(directory)
+
+    # Create the "image_mask" folder if it doesn't exist
+    mask_folder = directory / "image_mask"
+    mask_folder.mkdir(parents=True, exist_ok=True)
+
+    # Find all image files in the directory
+    image_files = [
+        f for f in directory.iterdir() if f.suffix.lower() in {".png", ".jpg", ".jpeg"}
+    ]
+
+    # Perform segmentation and save the results
+    for image_file in image_files:
+        image = Image.open(image_file).resize((256, 256))
+        image_array = np.expand_dims(image, 0) / 255.0
+        segmentation = unet_model.predict(image_array)
+
+        # Save the mask segmentation image
+        mask_image = (segmentation[0, :, :, 0] * 255).astype(np.uint8)
+        save_path = mask_folder / f"{image_file.stem}_mask.png"
+        pil_image = Image.fromarray(mask_image)
+        pil_image.save(save_path)
+        print(f"Saved mask segmentation for {image_file.name}")
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python inference_script.py <directory_path>")
         sys.exit(1)
 
+    # directory_path = r"{}".format(sys.argv[1])
     directory_path = sys.argv[1]
     main(directory_path)
-
