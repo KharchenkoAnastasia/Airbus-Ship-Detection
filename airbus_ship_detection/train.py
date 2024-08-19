da4c53b869c81916cd1c1975688f23849186db40
KharchenkoAnastasia <kharchenko.13.08@gmail.com>
2024-08-20 02:10:03 +0300

    Update train.py

--- airbus_ship_detection/train.py
+++ airbus_ship_detection/train.py
@@ -1,26 +1,29 @@
-import os
+from pathlib import Path
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from airbus_ship_detection.data_processing import (
-    balance_data
+
+from airbus_ship_detection.data_exploration import (
+    ship_count_distribution,
+    show_example_data,
 )
-from airbus_ship_detection.data_exploration import ship_count_distribution, show_example_data
+from airbus_ship_detection.data_processing import balance_data
 from airbus_ship_detection.model import UNet
 
-TRAIN_V2 = r"D:\Images_Airbus_Ship_Detection\train_v2"
-SEGMENTATION =r"D:\Images_Airbus_Ship_Detection\train_ship_segmentations_v2.csv"
+TRAIN_V2 = Path(r"D:\Images_Airbus_Ship_Detection\train_v2")
+SEGMENTATION = Path(r"D:\Images_Airbus_Ship_Detection\train_ship_segmentations_v2.csv")
 
 
-def main():
+def main() -> None:
     # Load and inspect the data
-    train_v2 = os.listdir(TRAIN_V2)
+    train_v2 = list(TRAIN_V2.iterdir())
     print("Len train_v2:", len(train_v2))
     train_csv = pd.read_csv(SEGMENTATION)
     print(train_csv.head())
 
     # Example data
-    ImageId = '0a1a7f395.jpg'
-    show_example_data(ImageId, train_csv,TRAIN_V2)
+    ImageId = "0a1a7f395.jpg"
+    show_example_data(ImageId, train_csv, TRAIN_V2)
 
     # Image Ship Count Distribution
     ship_count_distribution(train_csv)
@@ -41,13 +44,10 @@ def main():
     unet_model = UNet()
     unet_model.compile_model()
 
-    unet_model.fit_model(train_csv, TRAIN_V2, valid_csv,TRAIN_V2, epochs)
+    unet_model.fit_model(train_csv, TRAIN_V2, valid_csv, TRAIN_V2, epochs)
     unet_model.plot_training_history()
     unet_model.save_model()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
